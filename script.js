5a3c0ec255097980673c265dbbdd8d2013a8e836
badr <dridakhbadr@gmail.com>
2024-03-15 18:44:03 +0100

    landing page

---
+++ script.js
@@ -0,0 +1,11 @@
+// Add JavaScript here
+document.addEventListener('click', function (event) {
+  const firework = document.createElement('div');
+  firework.className = 'firework';
+  firework.style.left = event.clientX + 'px';
+  firework.style.top = event.clientY + 'px';
+  document.body.appendChild(firework);
+  setTimeout(function () {
+    document.body.removeChild(firework);
+  }, 1000);
+});
